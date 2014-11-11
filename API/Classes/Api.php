0af3d3689206364c82e6acff091db743e0da65c2
shibinv@gmail.com <shibinv@gmail.com>
2014-11-11 12:43:39 -0600

    Added department to search request

--- API/Classes/Api.php
+++ API/Classes/Api.php
@@ -115,6 +115,8 @@ class Api {
      * based on input string
      */
     protected function Search($str) {
+        $str = str_replace(" ", "%", $str); // replace space with wildcard
+        
         $sql = "SELECT * FROM full_course WHERE "
                 . "course_name like '%$str%' OR "
                 . "course_number like '%$str%' OR "
