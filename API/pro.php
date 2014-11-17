3bf62dd9dd2474ef137738e69c685f3129bfae03
shibinv@gmail.com <shibinv@gmail.com>
2014-11-17 14:39:24 -0600

    Added production file(pro.php).

--- API/pro.php
+++ API/pro.php
@@ -3,10 +3,48 @@
  * @author: Navsquire Team Members
  */
 
+require_once('Classes/Api.php');
+require_once('Classes/Json.php');
+require_once('Classes/Xml.php');
 /*
- * Used for production API calls.
- * Will return only basic errors.
+ * Used to handle API calls in production.
+ * Will not return debugging information.
  */
 
-echo 'not yet implemented, use the test url';
+// display no errors for production
+//error_reporting(E_ALL);
+ini_set('display_errors', 0);
 
+// type of data to return (json | xml)
+$type = filter_input(INPUT_GET, 'type');
+
+// used to figure out what the request is for
+$request = filter_input(INPUT_GET, 'request');
+
+// value thats being passed in if needed
+$value = filter_input(INPUT_GET, 'value');
+
+// value thats being passed in if needed
+$value2 = filter_input(INPUT_GET, 'value2');
+
+// create API class
+$api = new Api();
+
+// set debug for testing
+$api->setDebug(false);
+
+// return json string back to client
+$api->Find($request, $value, $value2);
+
+// get the appropriate type of data
+if ($type == 'xml') {
+    // set the content type so document is displayed properly
+    header('Content-Type: text/xml');
+    $result = $api->getXml();  
+} else {
+    // content type not really needed
+    header('Content-Type: application/json');
+    $result = $api->getJson(); // default
+}
+
+echo $result; // send data back to client
