932f25b4b33b588165e0f38ce394a25be6340ea5
shibinv@gmail.com <shibinv@gmail.com>
2014-11-24 16:45:39 -0600

    Search not filtering bug.

--- API/Classes/Api.php
+++ API/Classes/Api.php
@@ -120,13 +120,13 @@ class Api {
         $str = str_replace(" ", "%",  $str); // replace space with wildcard
         
         $sql = "SELECT * FROM full_course WHERE "
-                . "course_name like '%$str%' OR "
-                . "course_number like '%$str%' OR "
-                . "fac_last_name like '%$str%' OR "
-                . "fac_first_name like '%$str%' OR "
-                . "room_number like '%$str%' OR "
-                . "dept_subject like '%$str%' OR "
-                . "class_number like '%$str%' "
+                . "course_name like '%".$str."%' OR "
+                . "course_number like '%".$str."%' OR "
+                . "fac_last_name like '%".$str."%' OR "
+                . "fac_first_name like '%".$str."%' OR "
+                . "room_number like '%".$str."%' OR "
+                . "dept_subject like '%".$str."%' OR "
+                . "class_number like '%".$str."%' "
                 . "ORDER BY class_number";
 
         $this->Query($sql);
